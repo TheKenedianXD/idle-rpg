f387bd7a67b3d039e195d0ff728cf8f2aba0cb9a
JohnnyBeanzz <undercarpet.games@gmail.com>
2025-09-05 18:47:54 +0200

    Initial commit

---
+++ app/frontend/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="btn">
+  hello world
+</div>
+</template>
---
+++ app/frontend/tailwind.config.cjs
@@ -0,0 +1,12 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{vue,ts}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['ui-sans-serif', 'system-ui'],
+            },
+        },
+    },
+    plugins: [],
+};
